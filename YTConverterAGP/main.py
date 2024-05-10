f362c85429944a2460af6073267c6d2301db65f0
ArnawGP <arnaugraumdl@gmail.com>
2024-05-10 12:16:08 +0200

    ª

--- YTConverterAGP/main.py
+++ YTConverterAGP/main.py
@@ -1,21 +1,21 @@
 from pytube import YouTube
 from moviepy.editor import *
 
-def descarregar_i_extreure_audio(url, nom_arxiu):
-    # Descarregar el vídeo de YouTube
+def DownloadAndExtract(url, fileName):
+    # Download the video
     yt = YouTube(url)
     video = yt.streams.filter(only_audio=True).first()
     video.download(filename='temp')
 
-    # Extreure l'àudio del vídeo
+    # Extract the audio
     video_clip = AudioFileClip('temp.mp4')
-    video_clip.write_audiofile(nom_arxiu)
+    video_clip.write_audiofile(fileName)
 
-    # Eliminar el vídeo temporal
+    # Remove the temp file
     os.remove('temp.mp4')
 
 if __name__ == "__main__":
     url = input("Introdueix la URL del vídeo de YouTube: ")
-    nom_arxiu = input("Introdueix el nom de l'arxiu d'àudio de sortida (incloent l'extensió): ")
+    fileName = input("Introdueix el nom de l'arxiu d'àudio de sortida (incloent l'extensió): ")
 
-    descarregar_i_extreure_audio(url, nom_arxiu)
+    DownloadAndExtract(url, fileName)
