601be24839a7625bdc958c75bef263af4786e818
ArnawGP <arnaugraumdl@gmail.com>
2024-05-15 17:48:41 +0200

    Translation
    - set the download directory

--- YTConverterAGP/main.py
+++ YTConverterAGP/main.py
@@ -1,25 +1,25 @@
 from pytube import YouTube
 from moviepy.editor import *
 
-def descarregar_audio_des_de_youtube(url, nom_arxiu):
+def downloadYTAudio(url, file_name):
     try:
-        # Descarregar el vídeo
+        # Download the YouTube video
         yt = YouTube(url)
-        # Obtenir la millor pista d'àudio
+        # Get the track
         audio = yt.streams.filter(only_audio=True).first()
-        # Descarregar l'àudio
+        # Download the audio
         audio_file = audio.download(filename="temp_audio")
-        # Convertir l'àudio a MP3
+        # Convert the audio to mp3
         clip = AudioFileClip(audio_file)
-        clip.write_audiofile(nom_arxiu + ".mp3")
+        clip.write_audiofile(file_name + ".mp3")
         clip.close()
-        # Eliminar l'arxiu temporal
+        # Delete temp file
         os.remove(audio_file)
-        print("S'ha descarregat l'àudio com a MP3 amb èxit!")
+        print("Your audio file is ready!")
     except Exception as e:
-        print("Hi ha hagut un error:", str(e))
+        print("ERROR:", str(e))
 
 if __name__ == "__main__":
-    url = input("Introdueix la URL del vídeo de YouTube: ")
-    nom_arxiu = input("Introdueix el nom per a l'arxiu d'àudio: ")
-    descarregar_audio_des_de_youtube(url, nom_arxiu)
+    url = input("Enter the YouTube URL: ")
+    file_name = input("Enter the name of the audio file: ")
+    downloadYTAudio(url, file_name)
